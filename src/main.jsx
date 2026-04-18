f8266ac306152bc7fabcfc42bc497242c77c548c
rupesh-raj-9142 <rajrupesh824143@gmail.com>
2026-04-19 01:14:19 +0530

    Configure for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthProvider>
     <ChakraProvider>
       <CartProvider>
-        <BrowserRouter>
+        <BrowserRouter basename="/croma">
           <App />
         </BrowserRouter>
       </CartProvider>
